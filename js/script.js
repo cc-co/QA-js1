489f68b26df3dd79d13b03a2581df5d275155a67
cc-co <cwychan@hotmail.co.uk>
2021-09-29 11:17:26 +0100

    add additional change

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@
 
 let totalMoney = 4000;
 let moneyPaidSoFar = 2348;
-let totalLeftToPay;
+let totalLeftToPay = totalMoney-moneyPaidSoFar;
 
 console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalMoney-moneyPaidSoFar}`)
+console.log(`The total bill is ${totalMoney} the remaining amount of money to be paid is ${totalLeftToPay}`)
