10970236d4c7fa88e3a8ad4b8bbefbd5e93beda7
cc-co <cwychan@hotmail.co.uk>
2021-09-29 10:14:00 +0100

    add js folder

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Hello!");
+
+const msg = "this is a message";
+
+console.log(msg);
+
+console.log(`%c` + msg, `color: green`);
+
+const name = "c";
+const sname = "c";
+const place = "uk";
+const horo = "scoripio";
+
+console.log(`%c` + name + sname + place + horo, `color: orange`);
+console.log(`%c` + sname, `font:fantasy`);
+console.log(`%c` + place, `font-weight: bold`);
+console.log(`%c` + horo, `background-color: black; color: orange; padding: 10px`);
